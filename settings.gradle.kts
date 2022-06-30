e90fd1b13ab1ab80eb4e7058385a84978215238a
Emil Kantis <emil.kantis@protonmail.com>
2022-06-30 11:22:02 +0200

    Include Kotest and Kotest-extensions-spring into the build

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,11 @@
 rootProject.name = "kotest-examples-spring-webflux"
+
+if (File("../kotest-extensions-spring").exists() && System.getenv("NO_INCLUDE") == null) {
+   println("w: Including [kotest-extensions-spring] build")
+   includeBuild("../kotest-extensions-spring")
+}
+
+if (File("../kotest").exists() && System.getenv("NO_INCLUDE") == null) {
+   println("w: Including Kotest build, will substitute all Kotest stuff for currently checked out implementations")
+   includeBuild("../kotest")
+}
